cac129eba8818eef9152dfb457a3c44db7d32ae8
Sergey Misko <spy_3462@mail.ru>
2022-08-22 15:15:45 +0400

    Add project files.

---
+++ PLINQDataProcessingWithCancellation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PLINQDataProcessingWithCancellation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
